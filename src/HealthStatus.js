53159a846d1e420ef331934afb0774962a4dd7f0
Linda <linda.andersson93@gmail.com>
2020-12-04 07:38:58 +1100

    updated css

--- src/HealthStatus.js
+++ src/HealthStatus.js
@@ -8,14 +8,16 @@ export default function HealthStatus({hunger, boredom, dirty, toilet, healthPoin
     return(
         <aside className="health-status">
             <h2>Health status: {healthPoints}</h2>
-            <h3>Hunger:</h3>
-            <HealthBar status = {hunger} />
-            <h3>Bored:</h3>
-            <HealthBar status = {boredom} />
-            <h3>Dirty:</h3>
-            <HealthBar status = {dirty} />
-            <h3>Toilet:</h3>
-            <HealthBar status = {toilet} />
+            <div className="health-bars-container">
+                <h3>Hunger:</h3>
+                <HealthBar status = {hunger} />
+                <h3>Bored:</h3>
+                <HealthBar status = {boredom} />
+                <h3>Dirty:</h3>
+                <HealthBar status = {dirty} />
+                <h3>Toilet:</h3>
+                <HealthBar status = {toilet} />
+            </div>
         </aside>
     )
 }
